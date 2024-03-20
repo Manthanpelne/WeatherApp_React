e504b611fc645de79b78f06918e4c5b37fe34a22
Manthan Kailash Pelne <111408525+Manthanpelne@users.noreply.github.com>
2024-03-20 17:52:37 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import "../src/App.css"
+import { useEffect, useState } from "react"
+
+// let API_KEY =process.env.WEATHER_API_KEY
+// console.log(API_KEY)
+
+ export default function App() {
+  
+
+  return (
+    <div className="outerdiv">
+    home page
+    </div>
+  )
+}
